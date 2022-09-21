f8d0da4134536db181fc6524b2653e4a9d292f29
Ujaval Gandhi <ujaval@spatialthoughts.com>
2022-09-21 16:42:21 +0530

    rename app

--- streamlit/mapping_dashboard/app.py
+++ streamlit/mapping_dashboard/app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-import folium
-import leafmap.foliumap as leafmap
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
+import leafmap.foliumap as leafmap
 
-st.set_page_config(page_title="Dashboard", layout="wide")
+st.set_page_config(page_title='Dashboard', layout='wide')
 
 st.title('National Highway Dashboard')
 
-st.sidebar.title("About")
-st.sidebar.info('Explore the Roads')
+st.sidebar.title('About')
+st.sidebar.info('Explore the Highway Statistics')
+
 
 data_url = 'https://github.com/spatialthoughts/python-dataviz-web/raw/main/data/osm/'
 gpkg_file = 'karnataka.gpkg'
@@ -25,18 +25,17 @@ def read_gdf(url, layer):
 def read_csv(url):
     df = pd.read_csv(url)
     return df
-    
-   
+
 gpkg_url = data_url + gpkg_file
 csv_url = data_url + csv_file
 districts_gdf = read_gdf(gpkg_url, 'karnataka_districts')
 roads_gdf = read_gdf(gpkg_url, 'karnataka_highways')
 lengths_df = read_csv(csv_url)
 
-
+# Create the chart
 districts = districts_gdf.DISTRICT.values
 district = st.sidebar.selectbox('Select a District', districts)
-
+overlay = st.sidebar.checkbox('Overlay roads')
 district_lengths = lengths_df[lengths_df['DISTRICT'] == district]
 
 fig, ax = plt.subplots(1, 1)
@@ -45,29 +44,41 @@ district_lengths.plot(kind='bar', ax=ax, color=['blue', 'red'],
 ax.get_xaxis().set_ticklabels([])
 stats = st.sidebar.pyplot(fig)
 
+## Create the map
+
 m = leafmap.Map(
     layers_control=True,
     draw_control=False,
     measure_control=False,
     fullscreen_control=False,
 )
-
+m.add_basemap('CartoDB.DarkMatter')
 m.add_gdf(
     gdf=districts_gdf,
+    zoom_to_layer=False,
     layer_name='districts',
-    zoom_to_layer=True,
     info_mode='on_click',
-    style={'color': 'black', 'fillOpacity': 0.3, 'weight': 0.5},
+    style={'color': '#7fcdbb', 'fillOpacity': 0.3, 'weight': 0.5},
     )
 
+if overlay:
+    m.add_gdf(
+        gdf=roads_gdf,
+        zoom_to_layer=False,
+        layer_name='highways',
+        info_mode=None,
+        style={'color': '#225ea8', 'weight': 1.5},
+    )
+    
 selected_gdf = districts_gdf[districts_gdf['DISTRICT'] == district]
 
 m.add_gdf(
     gdf=selected_gdf,
     layer_name='selected',
-    zoom_to_layer=False,
+    zoom_to_layer=True,
     info_mode=None,
     style={'color': 'yellow', 'fill': None, 'weight': 2}
  )
 
-m_streamlit = m.to_streamlit(500, 500)
+
+m_streamlit = m.to_streamlit(600, 600)
